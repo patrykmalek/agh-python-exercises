679a12d4bb41353b35b23b3d912a46888c9940bc
Patryk Małek <patrykm5341@gmail.com>
2023-04-08 20:42:22 +0200

    Added simple mockups of function for Library

---
+++ library-management-system/Library.py
@@ -0,0 +1,13 @@
+class Library:
+
+    def add_book(self):
+        print("Adding book...")
+
+    def remove_book(self):
+        print("Removing book...")
+
+    def borrow_book(self):
+        print("Borrowing book...")
+
+    def return_book(self):
+        print("Returning book...")
