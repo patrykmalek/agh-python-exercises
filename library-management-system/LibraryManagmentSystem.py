75f0bfe5b1cd125bce6e8fcd8fc03e65264f0a13
Patryk Małek <patrykm5341@gmail.com>
2023-04-08 22:17:03 +0200

    Added some librarian menus

--- library-management-system/LibraryManagmentSystem.py
+++ library-management-system/LibraryManagmentSystem.py
@@ -4,15 +4,23 @@ from Library import Library
 
 library = Library()
 
-book_management = Menu("Zarządzanie książkami", [
+books_management_by_librarian = Menu("Zarządzanie książkami", [
+    Option("Wyświetl książki", library.display_books),
+    Option("Wyszukaj książkę", library.search_book),
     Option("Dodaj książkę", library.add_book),
     Option("Usuń książkę", library.remove_book)
 ])
 
-main_menu = Menu("Główne menu", [
-    Option("Wypożycz książkę", library.borrow_book),
-    Option("Zwróć książkę", library.return_book),
-    OptionMenu("Zarządzanie książkami", None,  book_management),
+readers_management_by_librarian = Menu("Zarządzanie czytelnikami", [
+    Option("Wyszukaj czytelnika", library.search_reader),
+    Option("Dodaj czytelnika", library.add_reader),
+    Option("Usuń czytelnika", library.remove_reader)
 ])
 
-main_menu.execute_menu()
+librarian_main_menu = Menu("Główne menu", [
+    Option("Wypożycz książkę", library.borrow_book),
+    OptionMenu("Zarządzanie książkami", None,  books_management_by_librarian),
+    OptionMenu("Zarządzanie czytelnikami", None,  readers_management_by_librarian),
+])
+
+librarian_main_menu.execute_menu()
