e9520b7e61b8c314a6885c86dac06fe38c56fa3f
Patryk Małek <patrykm5341@gmail.com>
2023-04-10 00:46:39 +0200

    fix search filter

--- library-management-system/SearchFilter.py
+++ library-management-system/SearchFilter.py
@@ -1,5 +1,5 @@
 class SearchFilter:
     @staticmethod
     def filter_books_by_title_and_author(books_option, query):
-        return list(filter(lambda x: query in x.obj_instance.title.lower() or query in x.obj_instance.author,
+        return list(filter(lambda x: query in x.obj_instance.title.lower() or query in x.obj_instance.author.lower(),
                            books_option))
