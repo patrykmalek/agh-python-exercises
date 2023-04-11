f5be5e98fee01586b0e2b25b07980dd67e690c96
Patryk Małek <81641561+patrykm5341@users.noreply.github.com>
2023-04-11 13:47:11 +0200

    Added filter to filter books

--- library-management-system/SearchFilter.py
+++ library-management-system/SearchFilter.py
@@ -3,3 +3,15 @@ class SearchFilter:
     def filter_books_by_title_and_author(books_option, query):
         return list(filter(lambda x: query in x.obj_instance.title.lower() or query in x.obj_instance.author.lower(),
                            books_option))
+    @staticmethod
+    def filter_borrowed_books(books_list):
+        return list(filter(lambda x: x.is_borrowed is True and x.borrowed_by is not None,
+                           books_list))
+    @staticmethod
+    def filter_reserved_books(books_list):
+        return list(filter(lambda x: x.is_reserved is True and x.reserved_by is not None,
+                           books_list))
+    @staticmethod
+    def filter_awaiting_to_return_books(books_list):
+        return list(filter(lambda x: x.to_return is True and x.is_borrowed is True and x.borrowed_by is not None,
+                           books_list))
